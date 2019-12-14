67d35da0c11a5d1c60127b73581d3a5b351ca944
Muhammed Emin Topuz <muhammed-e.72@hotmail.com>
2019-12-14 04:30:41 +0300

    update on dpinit

--- dbinit.py
+++ dbinit.py
@@ -6,7 +6,7 @@ import psycopg2 as dbapi2
 
 INIT_STATEMENTS = [
     """
-    CREATE TABLE IF NOT EXISTS Author( 
+        CREATE TABLE IF NOT EXISTS Author( 
                       AuthorID SERIAL PRIMARY KEY , 
                       name VARCHAR(30), 
                       surname VARCHAR(30),
@@ -66,7 +66,8 @@ INIT_STATEMENTS = [
                      );
 
     ALTER TABLE BookComment ADD COLUMN UserID INTEGER REFERENCES Users (UserID);
-    ALTER TABLE BookComment ADD COLUMN BookID INTEGER REFERENCES Books (BookID);       
+    ALTER TABLE BookComment ADD COLUMN BookID INTEGER REFERENCES Books (BookID);
+    ALTER TABLE BookComment ADD COLUMN DislikeNum INTEGER DEFAULT 0;       
     
     INSERT INTO Users (name,surname, email,password,isAdmin) 
     VALUES ('admin','admin','admin@gmail.com', 'gAAAAABd9BaEELg95qbxr7i1H-bnoUGyjGnEBYjAnVOpXEZFvwCdUoDzPuIgny3W1ou9JwwiR-WeIv0YgPU21OKI7T2Tg5wgCA==',1);
