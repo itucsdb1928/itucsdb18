cf452dca87af27e6340c3100a4c1a4bccfc65afd
alihantutuk <tutuk4354@gmail.com>
2019-12-15 21:36:15 +0300

    TRY RESTORE dbinit again

--- dbinit.py
+++ dbinit.py
@@ -38,8 +38,8 @@ INIT_STATEMENTS = [
                       Title VARCHAR(20),
                       PostDate  DATE DEFAULT CURRENT_DATE,
                       PageNum INTEGER,
-                      PublisherID INTEGER  REFERENCES Publisher (PublisherID)  , 
-                      AuthorID INTEGER  REFERENCES Author (AuthorID),
+                      PublisherID INTEGER  REFERENCES Publisher (PublisherID) SET NULL , 
+                      AuthorID INTEGER  REFERENCES Author (AuthorID) SET NULL,
                       Content VARCHAR(500),
                       BookReview INTEGER DEFAULT 0
                      );  
