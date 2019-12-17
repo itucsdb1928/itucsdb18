ad952c6e451c0141f5251cc40107da721681dfe1
Memin <topuzm15@gmail.com>
2019-12-17 08:46:15 +0300

    The last updates.

--- cyripto.py
+++ cyripto.py
@@ -1,10 +1,9 @@
 from cryptography.fernet import Fernet
-import os
 
 class Crypto:
     def __init__(self):
         # This one going to be environment variable
-        self.secretKey = os.getenv("SECRET_KEY")
+        self.secretKey = 'DN43aCpG04E83HPE963RiAe-sZ7uKaGiNPBnfdPcn3Q='
         self.f = Fernet(self.secretKey)
 
     def password2secret(self,password):
